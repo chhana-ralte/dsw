7f3704bb4d2372aec55e53b94fc2eb1b8bf4a928
blalmal10a <malsawma7777@gmail.com>
2025-03-18 14:01:23 +0530

    fix: vacant room route

--- resources/views/common/hostel/show.blade.php
+++ resources/views/common/hostel/show.blade.php
@@ -7,36 +7,41 @@
             Summary
             <table class="table table-hover table-striped">
                 <tr>
-                    <td>Total number of rooms</td><td>{{ $rooms->count() }}</td>
+                    <td>Total number of rooms</td>
+                    <td>{{ $rooms->count() }}</td>
                 </tr>
-                    <td>Total seats available for allocation</td><td>{{ $seats->sum('available') }}</td>
+                <td>Total seats available for allocation</td>
+                <td>{{ $seats->sum('available') }}</td>
                 </tr>
-                    <td>Total number of students allotted</td><td>{{ $allotted_seats->count() }}</td>
+                <td>Total number of students allotted</td>
+                <td>{{ $allotted_seats->count() }}</td>
                 </tr>
-                    <td>Total number of seats vacant</td><td>{{ $seats->sum('available')-$allotted_seats->count() }}</td>
+                <td>Total number of seats vacant</td>
+                <td>{{ $seats->sum('available') - $allotted_seats->count() }}</td>
                 </tr>
-                    <td>No. of students who are not allotted seat/room</td><td>{{ $unallotted_seats->count() }}</td>
+                <td>No. of students who are not allotted seat/room</td>
+                <td>{{ $unallotted_seats->count() }}</td>
                 </tr>
             </table>
         </x-block>
-        
+
         <x-block>
             <x-slot name="heading">
                 Menu
             </x-slot>
 
             <section id="Menu">
-                <div class="container">     
-                    <div class="row mb-4">
+                <div class="container">
+                    <div class="mb-4 row">
                         <a class="col-sm-3 bg-primary" href="/hostel/{{ $hostel->id }}/room">
-                            <div class="col col-md-4 bg-primary  mx-auto">
+                            <div class="mx-auto col col-md-4 bg-primary">
                                 <span class="btn btn-primary">
                                     <h4>Rooms</h4>
                                 </span>
                             </div>
                         </a>
                         <a class="col-sm-3 bg-secondary" href="/hostel/{{ $hostel->id }}/occupants?allot_seats=1">
-                            <div class="col col-md-4 mx-auto">
+                            <div class="mx-auto col col-md-4">
                                 <span class="btn btn-secondary">
                                     <h4>Occupants</h4>
                                 </span>
@@ -44,16 +49,16 @@
                         </a>
                     </div>
 
-                    <div class="row mb-4">
-                        <a class="col-sm-3 bg-primary" href="/hostel/{{ $hostel->id }}/room">
-                            <div class="col col-md-4 bg-primary  mx-auto">
+                    <div class="mb-4 row">
+                        <a class="col-sm-3 bg-primary" href="/hostel/{{ $hostel->id }}/room?status=vacant">
+                            <div class="mx-auto col col-md-4 bg-primary">
                                 <span class="btn btn-primary">
                                     <h4>Vacant Rooms</h4>
                                 </span>
                             </div>
                         </a>
                         <a class="col-sm-3 bg-secondary" href="/hostel/{{ $hostel->id }}/occupants?allot_seats=0">
-                            <div class="col col-md-4 mx-auto">
+                            <div class="mx-auto col col-md-4">
                                 <span class="btn btn-secondary">
                                     <h4>Students having no room</h4>
                                 </span>
