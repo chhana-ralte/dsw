ed83c9e1bfc313d3d3bda27179f631b681074b3b
Chhana <chhana.mizo@gmail.com>
2025-04-22 15:12:26 +0530

    Signed-off-by: Chhana <chhana.mizo@gmail.com>

--- app/Http/Controllers/AllotHostelController.php
+++ app/Http/Controllers/AllotHostelController.php
@@ -9,30 +9,33 @@ use App\Models\AllotSeat;
 
 class AllotHostelController extends Controller
 {
-    public function index(Allotment $allotment){
+    public function index(Allotment $allotment)
+    {
         return "Hostel allotment details";
     }
 
-    public function create(Allotment $allotment){
+    public function create(Allotment $allotment)
+    {
         $data = [
             'allotment' => $allotment,
-            'allot_hostels' => AllotHostel::where('allotment_id',$allotment->id)->orderBy('valid')->orderBy('id')->get(),
+            'allot_hostels' => AllotHostel::where('allotment_id', $allotment->id)->orderBy('valid')->orderBy('id')->get(),
         ];
-        return view('common.allot_hostel.create',$data);
-        return "Hostel allotment details";
+        return view('common.allot_hostel.create', $data);
+        // return "Hostel allotment details";
     }
 
-    public function store(Request $request,Allotment $allotment){
-        // return $request;
-        $allot_hostels = AllotHostel::where('allotment_id',$allotment->id);
-        $allot_seats = AllotSeat::whereIn('allot_hostel_id',$allot_hostels->pluck('id'));
+    public function store(Request $request, Allotment $allotment)
+    {
+        // return $allotment;
+        $allot_hostels = AllotHostel::where('allotment_id', $allotment->id);
+        $allot_seats = AllotSeat::whereIn('allot_hostel_id', $allot_hostels->pluck('id'));
 
-        $allot_seats->where('valid',1)->update([
+        $allot_seats->where('valid', 1)->update([
             'valid' => 0,
             'to_dt' => date('Y-m-d'),
         ]);
 
-        $allot_hostels->where('valid',1)->update([
+        $allot_hostels->where('valid', 1)->update([
             'valid' => 0,
             'to_dt' => date('Y-m-d'),
             'leave_dt' => date('Y-m-d'),
@@ -49,18 +52,17 @@ class AllotHostelController extends Controller
             ->with(['message' => ['type' => 'info', 'text' => 'Hostel changed']]);
     }
 
-    public function show(AllotHostel $allot_hostel){
-        if(isset(request()->back_link)){
+    public function show(AllotHostel $allot_hostel)
+    {
+        if (isset(request()->back_link)) {
             $back_link = request()->back_link;
-            
-        }
-        else{
+        } else {
             $back_link = "/hostel/" . $allot_hostel->hostel->id . "/occupants";
         }
         $data = [
             'back_link' => $back_link,
             'allot_hostel' => $allot_hostel
         ];
-        return view('common.allot_hostel.show',$data);
+        return view('common.allot_hostel.show', $data);
     }
 }
