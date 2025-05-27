5f1ebdff7b357a445e896b7b7dc7982d9cf439b9
Chhana Ralte <chhana.mizo@gmail.com>
2025-05-27 12:01:50 +0530

    Signed-off-by: Chhana Ralte <chhana.mizo@gmail.com>

--- resources/views/common/warden/list.blade.php
+++ resources/views/common/warden/list.blade.php
@@ -5,30 +5,49 @@
                 List of Wardens
             </x-slot>
 
-            @if(count($wardens) > 0)
+            @if (count($wardens) > 0)
                 <table class="table">
                     <tr>
                         <th>Hostel</th>
                         <th>Warden</th>
-                        <th>From</th>
-                        <th>To</th>
-                        @if(auth()->user() && auth()->user()->isAdmin())
+                        <th>Contact</th>
+                        @if (auth()->user() && auth()->user()->max_role_level() >= 2)
+                            <th>From</th>
+                            <th>To</th>
+                        @endif
+                        @if (auth()->user() && auth()->user()->isAdmin())
                             <th>User</th>
                         @endif
                     </tr>
 
-                    @foreach($wardens as $wd)
+                    @foreach ($wardens as $wd)
                         <tr>
                             <td>{{ $wd->hostel->name }}</td>
                             <td>{{ $wd->person->name }}</td>
-                            <td>{{ $wd->from_dt }}</td>
-                            <td>{{ $wd->to_dt }}</td>
-                            @can('manage',$wd)
+                            <td>
+                                @if ($wd->person->mobile)
+                                    Mobile: <a href="tel:{{ $wd->person->mobile }}">{{ $wd->person->mobile }}</a>
+                                @else
+                                    Not provided
+                                @endif
+                                @if ($wd->person->email)
+                                    <br>
+                                    email: <a href="mailto:{{ $wd->person->email }}">{{ $wd->person->email }}</a>
+                                @else
+                                    Not provided
+                                @endif
+                            </td>
+                            @if (auth()->user() && auth()->user()->max_role_level() >= 2)
+                                <td>{{ $wd->from_dt }}</td>
+                                <td>{{ $wd->to_dt }}</td>
+                            @endif
+                            @can('manage', $wd)
                                 <td>
-                                    @if($wd->user())
+                                    @if ($wd->user())
                                         <a href="/">{{ $wd->user()->username }}</a>
                                     @else
-                                        <a class="btn btn-primary btn-sm" href="/user/create?type=warden&id={{ $wd->id }}">Create user</a>
+                                        <a class="btn btn-primary btn-sm"
+                                            href="/user/create?type=warden&id={{ $wd->id }}">Create user</a>
                                     @endif
                                 </td>
                             @endcan
