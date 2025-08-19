2af3e56907c1d07671fb9e2111cde0e880868324
Chhana Ralte <chhana.mizo@gmail.com>
2025-08-19 17:11:29 +0530

    Signed-off-by: Chhana Ralte <chhana.mizo@gmail.com>

--- app/Http/Controllers/DikteiController.php
+++ app/Http/Controllers/DikteiController.php
@@ -28,19 +28,21 @@ class DikteiController extends Controller
 
     public function entry()
     {
-        if(request()->get('mzuid')){
+        if (request()->get('mzuid')) {
             $zirlais = Zirlai::where('mzuid', request()->get('mzuid'))->get();
-            if(count($zirlais) == 1){
-                return redirect('/diktei/option?zirlai_id=' . $zirlais->first()->id . '&mzuid=' . $zirlais->first()->mzuid) ;
-            }
-            else {
+            if (count($zirlais) == 1) {
+                return redirect('/diktei/option?zirlai_id=' . $zirlais->first()->id . '&mzuid=' . $zirlais->first()->mzuid);
+            } else {
+                $zirlais = Zirlai::where('rollno', request()->get('mzuid'))->get();
+                if (count($zirlais) == 1) {
+                    return redirect('/diktei/option?zirlai_id=' . $zirlais->first()->id . '&mzuid=' . $zirlais->first()->rollno);
+                }
                 $data = [
                     'mzuid' => request()->get('mzuid'),
                     'zirlais' => $zirlais,
                 ];
             }
-        }
-        else{
+        } else {
             $data = [
                 'mzuid' => '',
                 'zirlais' => [],
@@ -53,14 +55,13 @@ class DikteiController extends Controller
     {
         $zirlai = Zirlai::findOrFail(request()->get('zirlai_id'));
 
-        if(count($zirlai->dikteis) > 0){
+        if (count($zirlai->dikteis) > 0) {
             $data = [
                 'status' => 'submitted',
                 'zirlai' => $zirlai,
                 'dikteis' => Diktei::where('zirlai_id', $zirlai->id)->orderBy('serial')->get(),
             ];
-        }
-        else{
+        } else {
             $data = [
                 'status' => 'success',
                 'zirlai' => $zirlai,
@@ -74,28 +75,28 @@ class DikteiController extends Controller
     {
         // return request()->all();
 
-        foreach(request()->subject as $key => $val){
+        foreach (request()->subject as $key => $val) {
             Diktei::updateOrCreate([
                 'zirlai_id' => request()->zirlai_id,
-                'serial' => $key+1,
-            ],[
+                'serial' => $key + 1,
+            ], [
                 'zirlai_id' => request()->zirlai_id,
-                'serial' => $key+1,
+                'serial' => $key + 1,
                 'subject_id' => $val,
             ]);
         }
         return redirect('/diktei/option?zirlai_id=' . request()->zirlai_id);
     }
 
-    public function subject_allotments(){
+    public function subject_allotments()
+    {
         $subjects = Subject::select('subjects.*')->join('courses', 'courses.id', 'subjects.course_id')
             ->where('courses.cbcs', 1)->get();
         // return $subjects;
-        if(request()->get('subject_id')){
+        if (request()->get('subject_id')) {
             $dtallots = Dtallot::where('subject_id', request()->get('subject_id'))->orderBy('zirlai_id')->get();
             $subject = Subject::find(request()->get('subject_id'));
-        }
-        else{
+        } else {
             $subject = false;
             $dtallots = [];
         }
@@ -106,13 +107,14 @@ class DikteiController extends Controller
         ];
         return view('diktei.dtallot', $data);
     }
-    public function allot_subjects(){
+    public function allot_subjects()
+    {
         $zirlais = Zirlai::select('zirlais.*')->join('dikteis', 'dikteis.zirlai_id', 'zirlais.id')->where('dikteis.serial', 1)->orderBy('created_at')->get();
         Dtallot::truncate();
-        foreach($zirlais as $zl){
-            foreach(Diktei::where('zirlai_id', $zl->id)->orderBy('serial')->get() as $dt){
+        foreach ($zirlais as $zl) {
+            foreach (Diktei::where('zirlai_id', $zl->id)->orderBy('serial')->get() as $dt) {
                 $subject = Subject::where('id', $dt->subject_id)->first();
-                if($subject->capacity > Diktei::where('subject_id', $subject->id)->count()){
+                if ($subject->capacity > Diktei::where('subject_id', $subject->id)->count()) {
                     Dtallot::create([
                         'zirlai_id' => $zl->id,
                         'subject_id' => $subject->id,
@@ -125,7 +127,8 @@ class DikteiController extends Controller
         return redirect('/diktei/dtallot')->with(['message' => ['type' => 'info', 'text' => "Successful"]]);
     }
 
-    public function clear_options(){
+    public function clear_options()
+    {
         $zirlai = Zirlai::findOrFail(request()->post('zirlai_id'));
 
         Diktei::where('zirlai_id', $zirlai->id)->delete();
