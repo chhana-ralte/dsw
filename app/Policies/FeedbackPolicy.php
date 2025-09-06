ea6cb56e096700fc434c90a39e93b81efef5b649
Chhana Ralte <chhana.mizo@gmail.com>
2025-09-06 21:42:15 +0530

    Signed-off-by: Chhana Ralte <chhana.mizo@gmail.com>

--- app/Policies/FeedbackPolicy.php
+++ app/Policies/FeedbackPolicy.php
@@ -17,15 +17,12 @@ class FeedbackPolicy
 
     public function gives(User $user):bool
     {
-        $users = DB::select("SELECT users.*
-            FROM users JOIN role_user ON users.id = role_user.user_id AND role_user.type = 'warden'
-            JOIN wardens ON wardens.id = role_user.foreign_id AND wardens.valid = 1
-            WHERE users.id = " . $user->id );
+        return $user->max_role_level >= 3;
 
-        // $users = DB::select("SELECT users.*
-        //     FROM users JOIN role_user ON users.id = role_user.user_id AND role_user.type = 'allotment'
-        //     JOIN allotments ON allotments.id = role_user.foreign_id AND allotments.valid = 1
-        //     WHERE users.id = " . $user->id );
+        $users = DB::select("SELECT users.*
+            FROM users JOIN role_user ON users.id = role_user.user_id AND role_user.type = 'allotment'
+            JOIN allotments ON allotments.id = role_user.foreign_id AND allotments.valid = 1
+            WHERE users.id = " . $user->id );
         if(count($users) > 0){
             return true;
         }
@@ -36,6 +33,6 @@ class FeedbackPolicy
 
     public function manages(User $user):bool
     {
-        return auth()->user()->max_role_level() >= 3;
+        return $user->max_role_level() >= 3;
     }
 }
--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -23,17 +23,7 @@
             <p>
                 Only those who experience the mess during the aforesaid may give the feedback.
             </p>
-            <div>
-                <h4>Instructions:</h4>
-                <ul>
-                    <li>You are about to give feedback on {{ $feedback_master->title }}
-                    <li>Each item is to be rated from 0 to 10 using the sliders.</li>
-                    <li>The feedback has to be from your own opinion.</li>
-                    <li>You can give only one feedback.</li>
-                    <li>Your feedback can not be traced back to you.</li>
-                </ul>
-                <a class="btn btn-primary" href="/feedbackMaster/{{ $feedback_master->id }}/feedback/create">Proceed>></a>
-            </div>
+
         </x-block>
     </x-container>
 </x-layout>
