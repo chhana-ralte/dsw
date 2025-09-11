0b4dbb498c1981b2a6f73ea0f84383568e7b7dd0
Chhana Ralte <chhana.mizo@gmail.com>
2025-09-11 15:12:07 +0530

    Signed-off-by: Chhana Ralte <chhana.mizo@gmail.com>

--- resources/views/components/block.blade.php
+++ resources/views/components/block.blade.php
@@ -1,8 +1,10 @@
-<div {{ $attributes->merge(['class' => "my-3 mx-2 p-2 bg-light text-dark rounded border shadow" ]) }}>
-    @isset($heading)
-        <h3 class="text-lg font-medium text-gray-900 text-primary">
-            {{ $heading }}
-        </h3>
-    @endisset
-    {{ $slot }}
+<div {{ $attributes->merge(['class' => 'p-2']) }}>
+    <div class="p-2 my-2 border rounded shadow bg-light text-dark">
+        @isset($heading)
+            <h3 class="text-lg font-medium text-gray-900 text-primary">
+                {{ $heading }}
+            </h3>
+        @endisset
+        {{ $slot }}
+    </div>
 </div>
