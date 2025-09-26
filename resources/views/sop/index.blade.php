b327ae36993dd8f31f1c6681bc2833618212659f
Chhana <chhana.mizo@gmail.com>
2025-09-26 14:50:44 +0530

    Signed-off-by: Chhana <chhana.mizo@gmail.com>

--- resources/views/sop/index.blade.php
+++ resources/views/sop/index.blade.php
@@ -9,25 +9,30 @@
                 </p>
                 @endcan
             </x-slot>
-            @foreach($sops as $sop)
-                <div class="accordion" id="accordionExample">
+            
+            <div class="accordion" id="accordionExample">
+                @foreach($sops as $sop)
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="heading_{{ $sop->id }}">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sop_{{ $sop->id }}" aria-expanded="false" aria-controls="collapseTwo">
                                 <strong>{{ $sop->title }}</strong>
                             </button>
                         </h2>
-                        <div id="sop_{{ $sop->id }}" class="accordion-collapse collapse" aria-labelledby="heading_{{ $sop->id }}" data-bs-parent="#accordionExample">
+                        <div id="sop_{{ $sop->id }}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                {!! substr($sop->content, 0, 100) !!}
+                                {!! substr(strip_tags($sop->content),0,200) !!}
+                                @if(strlen(strip_tags($sop->content)) > 200)
+                                    ...
+                                @endif
                                 <p>
                                     <a href="/sop/{{ $sop->id }}">Details</a>
                                 </p>
                             </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                
+                @endforeach
+            </div>
             
 
 
