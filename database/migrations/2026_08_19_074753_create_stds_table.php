492b00b8d45e0bb357b10e5bc3453086a3bd25b6
chhana ralte <chhana.mizo@gmail.com>
2026-08-19 13:34:38 +0530

    Signed-off-by: chhana ralte <chhana.mizo@gmail.com>

---
+++ database/migrations/2026_08_19_074753_create_stds_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stds', function (Blueprint $table) {
+            $table->id();
+            $table->string('rollno',10);
+            $table->string('name',50);
+            $table->string('phone',20);
+            $table->string('email',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stds');
+    }
+};
